c1c187efd0ed1e9f2d2b4bd7e992460422ec2421
damiresgomes <damiresbrito2007@gmail.com>
2026-08-23 21:55:26 -0300

    modificacoes

--- config.php
+++ config.php
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-<?php
-    $host = "127.0.0.1";
-    $user = "britoestetica";
-    $pass = "brito28045";
-    $dbname = "brito_estetica";
-
-    try {
-        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        die("Erro ao conectar ao banco de dados: " . $e->getMessage());
-    }
-=======
 <?php
     $host = "127.0.0.1";
     $user = "britoestetica";
